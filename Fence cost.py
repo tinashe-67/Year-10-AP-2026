92db717b4ee81ecb75e8351f6f8331a1b001178e
tinashe-67 <tinashe.ndongwe@kingsway.school.nz>
2026-06-03 10:33:06 +1200

    Fence cost.py

--- Fence cost.py
+++ Fence cost.py
@@ -1,11 +1,31 @@
+def num_check(question):
+    error = "Please enter a number that is more than zero\n"
+    while True:
+
+        try:
+            responce = float(input(question))
+
+            if responce > 0:
+                break
+                
+
+            else:
+                print(error)
+                    
+        except ValueError:
+            print(error)
+
+    return responce
+
 keep_going = ""
 while keep_going == "":
-    width = float(input("What is the width of the fence: "))
-    length = float(input("what is the length of the fence: "))
+    width = num_check("What is the width of the fence: ")
+    length = num_check("what is the length of the fence: ")
     perimeter = 2 * (length + width)
-    cost_per_m = int(input("What is the cost per meter of fencing: "))
+    cost_per_m = num_check("What is the cost per meter of fencing: ")
     final_cost = cost_per_m * perimeter
     print()
-    print(f'the cost of {perimeter} meters of fencing is ${final_cost}0')
+    print(f'the cost of {perimeter} meters of fencing is ${final_cost}')
 
     keep_going = input("Press enter to go again, or any other key to quit")
+
